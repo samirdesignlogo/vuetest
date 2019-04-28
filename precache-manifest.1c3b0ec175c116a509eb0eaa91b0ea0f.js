e6728a0a5829a2f5b30b59710c45c12fc3ca7235
samirlogodesign <49608918+samirlogodesign@users.noreply.github.com>
2019-04-29 00:41:53 +0400

    Initial commit

---
+++ precache-manifest.1c3b0ec175c116a509eb0eaa91b0ea0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "197b89d5f53dffe9b52f",
+    "url": "/css/app.adfb3280.css"
+  },
+  {
+    "revision": "197b89d5f53dffe9b52f",
+    "url": "/js/app.ae6ec1c3.js"
+  },
+  {
+    "revision": "3adea0dc6d5ab1837fba",
+    "url": "/css/chunk-vendors.d7a9a903.css"
+  },
+  {
+    "revision": "3adea0dc6d5ab1837fba",
+    "url": "/js/chunk-vendors.0d0edd58.js"
+  },
+  {
+    "revision": "ff180fa4df87ec862373aff14190023c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
